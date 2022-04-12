7002342e03de7c16e12259cb728c14fd72a57097
Maria T. Andersen <mteilmann93@gmail.com>
2022-04-12 21:46:04 +0200

    firstly, create a header for the list

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+const listContainer = document.getElementById("list-container");
+const createListButton = document.getElementById("create-list-button");
+const inputItem = document.getElementById("input-item");
+
+createListButton.addEventListener("click", () => {
+  createHeader();
+});
+
+function createHeader() {
+  const header = document.createElement("div");
+  header.innerText = "To-do list";
+  listContainer.appendChild(header);
+}
