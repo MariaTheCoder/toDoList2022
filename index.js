cc217add013d840fb5fc8a38fe85575713a601b6
Maria T. Andersen <mteilmann93@gmail.com>
2022-04-12 22:26:10 +0200

    fix structural problem with function to create new items on the to-do list

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const createListButton = document.getElementById("create-list-button");
 const inputItem = document.getElementById("input-item");
 
 createListButton.addEventListener("click", () => {
-  createHeader();
+  createListItem();
 });
 
 function createHeader() {
   const header = document.createElement("div");
   header.innerText = "To-do list";
   listContainer.appendChild(header);
+}
+
+function createListItem() {
+  const list = document.getElementById("list");
+  const item = document.createElement("li");
+  item.innerText = inputItem.value;
+  list.appendChild(item);
 }
